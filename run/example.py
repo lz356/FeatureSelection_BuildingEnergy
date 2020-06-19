5f6ce4ecb44f0eeb9fcd64ee10ee6bbccf341c9f
lz356 <leonzhang2000@gmail.com>
2020-06-19 17:25:00 -0600

    Create example.py

---
+++ run/example.py
@@ -0,0 +1,21 @@
+#author:LiangZhang@NREL
+import pandas as pd
+import fsbe.base.hybrid_feature_selection_with_filter_and_wrapper as hfswfw
+
+# Example Run
+inputs_output_example = pd.read_csv('../data/example_data.csv')
+inputs_example = inputs_output_example.iloc[:, 0:-1]
+output_example = inputs_output_example.iloc[:, -1]
+
+selected_inputs_dataframe = hfswfw(
+    inputs = inputs_example,
+    output = output_example,
+    exemption_variable_list = ['feature_9_exemption'],
+    time_lag_variable_list = ['feature_10_time_lag_test_1','feature_11_time_lag_test_2'],
+    time_lag_steps = 2,
+    filter_method_irrelevancy_threshold = 0.1,
+    filter_method_redundancy_threshold = 0.1,
+    only_return_inputs_names = False,
+    print_procedures = True)
+
+print(selected_inputs_dataframe)
